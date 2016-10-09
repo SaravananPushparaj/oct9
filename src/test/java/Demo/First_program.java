d87b55dda577390efd108502f4cfa8916871bc4c
saravananpushparaj <saravananpushparaj@gmail.com>
2016-10-09 11:35:10 +0530

    V1.0

--- src/test/java/Demo/First_program.java
+++ src/test/java/Demo/First_program.java
@@ -5,9 +5,8 @@ public class First_program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello world");
-		System.out.println("Hi");
-
+		System.out.println("Olympics");
+		
 	}
 
 }
